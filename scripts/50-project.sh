c5571979d0bd7032fb3ec6112b1928f6469b6cd1
Madan Sapkota <contact@madansapkota.com>
2024-12-28 06:13:10 +0000

    Execute command as non-root user

--- scripts/50-project.sh
+++ scripts/50-project.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Exit on error
+set -e
+
+su - application <<'EOF'
+cd /var/www/html
+
 # Install dependencies
-composer install --prefer-dist --optimize-autoloader --no-progress --no-interaction
+/usr/local/bin/php /usr/local/bin/composer install --prefer-dist --optimize-autoloader --no-progress --no-interaction
+
+EOF
