c1f46a6bc5340557e7a7a3bc273e100b4286b08d
Madan Sapkota <contact@madansapkota.com>
2024-12-28 09:47:19 +0000

    Add rector config

---
+++ html/rector.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->parallel();
+
+    // Set custom paths
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    // Skips
+    $rectorConfig->skip([
+        // __DIR__ . '/app/...',
+    ]);
+
+    // Define rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        PHPUnitSetList::PHPUNIT_100,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::PRIVATIZATION,
+        SetList::TYPE_DECLARATION,
+    ]);
+};
