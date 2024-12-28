3f6d033abb85a4ae0947cba2194e0380ff26ef4e
Madan Sapkota <contact@madansapkota.com>
2024-12-28 09:09:40 +0000

    Add factory test cases

---
+++ html/tests/Unit/TotpFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use RemoteMerge\Totp\TotpException;
+use RemoteMerge\Totp\TotpFactory;
+use RemoteMerge\Totp\TotpInterface;
+
+final class TotpFactoryTest extends TestCase
+{
+    /**
+     * Test creating a default TOTP instance.
+     * @throws TotpException
+     */
+    public function test_create_default(): void
+    {
+        $totp = TotpFactory::create();
+        $this->assertInstanceOf(TotpInterface::class, $totp);
+    }
+
+    /**
+     * Test creating a configured TOTP instance.
+     * @throws TotpException
+     */
+    public function test_create_configured(): void
+    {
+        $totp = TotpFactory::create(['algorithm' => 'sha256', 'digits' => 8, 'period' => 60]);
+        $this->assertSame('sha256', $totp->getAlgorithm());
+        $this->assertSame(8, $totp->getDigits());
+        $this->assertSame(60, $totp->getPeriod());
+    }
+}
